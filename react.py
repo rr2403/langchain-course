4cd4d4fd8d88af036cdf3e5a807481005cc042d8
Rohit Ranjan <rohitranjanmgr@gmail.com>
2026-08-13 05:25:09 +0530

    Function calling reasoning

---
+++ react.py
@@ -0,0 +1,20 @@
+# For holding the reasoning engine
+# Function calling will be the reasoning engine
+
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+def triple(num:float) -> float:
+    """
+    param num: a number to triple
+    returns: the triple of the input number
+    """
+    return float(num) * 3
+
+tools = [TavilySearch(max_result=1), triple]
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)
