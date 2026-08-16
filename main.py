110b6b5cb8152f24ca9a3eaaaea70ad8fd1e7682
Rohit Ranjan <rohitranjanmgr@gmail.com>
2026-08-17 05:08:04 +0530

    Graph Execution

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from langgraph-course!")
+from dotenv import load_dotenv
+from langgraph.graph import MessagesState, StateGraph, END
+from node import run_agent_reasoning, tool_node
 
+load_dotenv()
 
+AGENT_REASON = "agent_reasoning"
+ACT = "act"
+LAST = -1
+
+def should_continue(state: MessagesState) -> str:
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+flow = StateGraph(MessagesState)
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+flow.add_conditional_edges(AGENT_REASON, should_continue, {
+    END:END,
+    ACT:ACT})
+flow.add_edge(ACT, AGENT_REASON)
+app=flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
 if __name__ == "__main__":
-    main()
+    print("Hello from Langgraph")
